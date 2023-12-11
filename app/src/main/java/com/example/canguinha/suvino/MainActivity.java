ca692ad665a8b73d73169582ea6d92afc0776014
thtech1 <roquelima182@gmail.com>
2023-12-11 17:19:36 -0300

    ajustes

--- app/src/main/java/com/example/canguinha/suvino/MainActivity.java
+++ app/src/main/java/com/example/canguinha/suvino/MainActivity.java
@@ -8,16 +8,20 @@ import android.view.View;
 
 import com.example.canguinha.suvino.activity.Cadastro;
 import com.example.canguinha.suvino.activity.Login;
+import com.example.canguinha.suvino.activity.Principal_activity;
+import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
+    private FirebaseAuth autentificacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
@@ -48,12 +52,27 @@ public class MainActivity extends IntroActivity {
                 .layout(R.layout.cadastro_)
                 .build());
     }
-    public void cadastrar(View view){
+
+    protected void onStart(){
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+    public void Cadastrar(View view){
         startActivity(new Intent(this, Cadastro.class));
 
-    }public void entrar(View view){
+    }public void Entrar(View view){
         startActivity(new Intent(this, Login.class));
 
+    }
+    public void  verificarUsuarioLogado(){
+        autentificacao = ConfiguracaoFirebase.getFireAutentificacao();
+                if (autentificacao.getCurrentUser() != null){
+                abrirTelaPrincipal();
+                }
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, Principal_activity.class));
 
     }
 }
